5314eceee71a68894c72989ae37f61c7ab3a7bb2
johnashu <johnashu1@hotmail.com>
2017-07-25 18:56:57 +0200

    enumerate

--- iterating.py
+++ iterating.py
@@ -34,3 +34,12 @@ print(next(googol))
 
 # Iterators as function arguments
 
+values = range(10, 21)
+print(values)
+
+values_list = list(values)
+print(values_list)
+
+values_sum = sum(values)
+print(values_sum)
+
