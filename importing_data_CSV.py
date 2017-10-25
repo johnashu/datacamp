abb93eede7ed3d520de78e61fb6cb671156e8e93
johnashu <johnashu1@hotmail.com>
2017-10-25 22:13:27 +0200

    .

--- importing_data_CSV.py
+++ importing_data_CSV.py
@@ -6,9 +6,5 @@ url = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datas
 urlretrieve(url, 'winequality-red.csv')
 
 df = pd.read_csv('winequality-red.csv', sep=';')
-print(df.head())
 
-pd.DataFrame.hist(df.ix[:, 0:1])
-plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
-plt.ylabel('count')
-plt.show()
+print(df.iloc[1:2])
